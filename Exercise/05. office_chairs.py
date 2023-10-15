a098c9dc2a2ff2885e5717000bc24dd525cd8394
me_madness <p_rangel@abv.bg>
2023-10-15 17:03:18 +0200

    05 all most finish

--- Exercise/05. office_chairs.py
+++ Exercise/05. office_chairs.py
@@ -6,14 +6,17 @@
 
 rooms = int(input())
 total_free_chairs = 0
+needed_chairs_in_room = 0
 for room in range(rooms):
     current_room = input().split()
     chair_in_room = current_room[0]
     visitors_in_room = current_room[1]
-    if len(chair_in_room) > visitors_in_room:
-        total_free_chairs += 1
-        
-    print(chair_in_room)
-    print(visitors_in_room)
-# print("{needed_chairs_in_room} more chairs needed in room {number_of_room}")
-print("Game On, {total_free_chairs} free chairs left")
+    if len(chair_in_room) > int(visitors_in_room):
+        total_free_chairs += len(chair_in_room) - int(visitors_in_room)
+    elif len(chair_in_room) < int(visitors_in_room):
+        needed_chairs_in_room += int(visitors_in_room) - len(chair_in_room)  
+        if needed_chairs_in_room: 
+            print(f"{needed_chairs_in_room} more chairs needed in room {room}")
+
+
+print(f"Game On, {total_free_chairs} free chairs left")
