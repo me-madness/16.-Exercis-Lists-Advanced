a99284db0a56ef18fdac0ed7952d4f90ecd736fc
me_madness <p_rangel@abv.bg>
2023-10-15 16:21:33 +0200

    04.

--- Exercise/04. number_classification.py
+++ Exercise/04. number_classification.py
@@ -3,3 +3,13 @@
 
 
 # Second way from me 
+
+numbers = input().split(", ")
+positive_numbers = [positive for positive in numbers if int(positive) >= 0]
+negative_numbers = [negative for negative in numbers if int(negative) < 0]
+even_numbers = [even for even in numbers if int(even) % 2 == 0]
+odd_numbers = [odd for odd in numbers if not int(odd) % 2 == 0]
+print(f"Positive: {', '.join(positive_numbers)}")
+print(f"Negative: {', '.join(negative_numbers)}")
+print(f"Even: {', '.join(even_numbers)}")
+print(f"Odd: {', '.join(odd_numbers)}")
