f6a249c6bff3265daa44834c70799db7f2724fbf
me_madness <p_rangel@abv.bg>
2023-10-15 16:33:40 +0200

    05

--- Exercise/05. office_chairs.py
+++ Exercise/05. office_chairs.py
@@ -3,3 +3,8 @@
 
 
 # Second way from me 
+
+rooms = int(input())
+
+print("{needed_chairs_in_room} more chairs needed in room {number_of_room}")
+print("Game On, {total_free_chairs} free chairs left")
