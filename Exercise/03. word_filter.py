692825d1fc8f901d4a137d249b86cb5b0d92344e
me_madness <p_rangel@abv.bg>
2023-10-15 15:44:43 +0200

    03. Done

--- Exercise/03. word_filter.py
+++ Exercise/03. word_filter.py
@@ -1,7 +1,11 @@
 # First way from the Lector
 
+words = input().split()
+filtered_words = [word for word in words if len(word) % 2 == 0]
+print("\n".join(filtered_words))
+
+# Second way from me 
+
 text = input().split(" ")
 new_text = [text_one for text_one in text if len(text_one) % 2 == 0]
 print("\n".join(new_text))
-
-# Second way from me 
