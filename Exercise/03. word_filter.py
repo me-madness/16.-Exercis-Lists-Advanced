3d5095e7f0b4d753b57bc7856c955e347cbd3d5d
me_madness <p_rangel@abv.bg>
2023-10-15 15:42:43 +0200

    03. It's done

--- Exercise/03. word_filter.py
+++ Exercise/03. word_filter.py
@@ -1,7 +1,7 @@
 # First way from the Lector
 
 text = input().split(" ")
-new_text = [tex for tex in text if tex % 2 == 0]
-print(f"{new_text}\n")
+new_text = [text_one for text_one in text if len(text_one) % 2 == 0]
+print("\n".join(new_text))
 
 # Second way from me 
