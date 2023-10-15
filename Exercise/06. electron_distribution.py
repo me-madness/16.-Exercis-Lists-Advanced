0f4d744c0a19b37bdc99e6a75985c8ae777bd7a8
me_madness <p_rangel@abv.bg>
2023-10-15 18:31:47 +0200

    06.

--- Exercise/06. electron_distribution.py
+++ Exercise/06. electron_distribution.py
@@ -1,5 +1,19 @@
 # First way from the Lector
 
-
+number_of_electrons = int(input())
+shells = []
+for shell in range(1, number_of_electrons + 1):
+    max_electrons_in_current_shell = 2 * shell ** 2
+    if number_of_electrons >= max_electrons_in_current_shell:
+        shells.append(max_electrons_in_current_shell)
+        number_of_electrons -= max_electrons_in_current_shell
+        if number_of_electrons == 0:
+            break
+    else:
+        shells.append(number_of_electrons)
+        break
+print(shells)
 
 # Second way from me 
+
+electrons = int(input())
