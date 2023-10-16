104a5eafa4128fb83e486b255c3459af7b94bf2e
me_madness <p_rangel@abv.bg>
2023-10-16 20:39:04 +0200

    11.in progress

--- Exercise/11. softuni_course_planning.py
+++ Exercise/11. softuni_course_planning.py
@@ -10,12 +10,12 @@ while command != "course start":
     if command[0] == "Add":
         list_with_exercise.pop(command[1])
     elif command[0] == "Insert":
-        
+        list_with_exercise.insert(command[2], command[1])
     elif command[0] == "Remove":
-        pass
+        list_with_exercise.remove(command[1])
     elif command[0] == "Swap":
         pass
     elif command[0] == "IExercise":
         pass    
     
-print(f"{lesson_index}.{lessonTitle}")    
+print(f"{list_with_exercise}.{list_with_exercise}")    
