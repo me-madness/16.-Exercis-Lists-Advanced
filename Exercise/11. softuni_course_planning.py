d38451845a53fdea40f85f6fd2bf1c0c3715e363
me_madness <p_rangel@abv.bg>
2023-10-23 21:01:47 +0200

    11.1

--- Exercise/11. softuni_course_planning.py
+++ Exercise/11. softuni_course_planning.py
@@ -7,18 +7,19 @@
 list_with_exercise = input().split(", ")
 
 while True:
-    command = input().split(":")
+    commands = input().split(":")
+    command = commands[0]
     if command == "course start":
         break
-    elif command[0] == "Add":
+    elif command == "Add":
         list_with_exercise.append(command[1])
-    elif command[0] == "Insert":
+    elif command == "Insert":
         list_with_exercise.insert(command[2], command[1])
-    elif command[0] == "Remove":
+    elif command == "Remove":
         list_with_exercise.remove(command[1])
-    elif command[0] == "Swap":
+    elif command == "Swap":
         pass
-    elif command[0] == "Exercise":
+    elif command == "Exercise":
         pass    
     
 print(f"{list_with_exercise}.{list_with_exercise}")    
