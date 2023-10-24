e01888a48c1190d57d825b7e88db57ca5bb88c85
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-24 21:43:33 +0200

    swap function it's done

--- Exercise/11. softuni_course_planning.py
+++ Exercise/11. softuni_course_planning.py
@@ -6,8 +6,8 @@
 
 list_with_exercise = input().split(", ")
 lesson_index = 0
-while True:
 
+while True:
     commands = input().split(":")
     command = commands[0]
     if command == "course start":
@@ -23,16 +23,27 @@ while True:
         index = commands[1]
         list_with_exercise.remove(index_one)
     elif command == "Swap":
-        pass
+        index_one = commands[1]
+        index_two = commands[2]
+        a, b = list_with_exercise.index(index_one), list_with_exercise.index(index_two)
+        list_with_exercise[b], list_with_exercise[a] = list_with_exercise[a], list_with_exercise[b]
     elif command == "Exercise":
         pass    
 
 
-# lesson_index = (exercise for exercise in len(list_with_exercise)) 
 for exercise in list_with_exercise:
     lesson_index += 1   
     print(f"{lesson_index}.{exercise}")  
 
+
+# "Swap:{lessonTitle}:{lessonTitle}" - swap the position of the two lessons if they exist.
+# a, b = i.index('password2'), i.index('password1')
+# i[b], i[a] = i[a], i[b]
+
+# "Exercise:{lessonTitle}" - add Exercise in the schedule right after the lesson index, 
+# if the lesson exists and there is no exercise already, in the following format 
+# "{lessonTitle}-Exercise". If the lesson doesn't exist, add the lesson at the end of 
+# the course schedule, followed by the Exercise.
 # Data Types, Objects, Lists
 # Add:Databases
 # Insert:Arrays:0
